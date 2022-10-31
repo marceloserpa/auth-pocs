79d8462d3fcb7d661a623ed5c13364e1fb08a773
marceloserpa <jdev.marcelo@gmail.com>
2022-10-31 19:47:34 -0300

    oauth2 poc: init

---
+++ oauth2-poc/src/test/java/com/marceloserpa/oauth2poc/Oauth2PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloserpa.oauth2poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
