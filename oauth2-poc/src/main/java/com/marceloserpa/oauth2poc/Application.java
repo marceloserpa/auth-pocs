c793fd75c308664ccb858b622f7a89aad9bab49c
marceloserpa <jdev.marcelo@gmail.com>
2022-10-31 20:57:52 -0300

    oauth2: integrate spring security app with keycloak

--- oauth2-poc/src/main/java/com/marceloserpa/oauth2poc/Application.java
+++ oauth2-poc/src/main/java/com/marceloserpa/oauth2poc/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Oauth2PocApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Oauth2PocApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
