871f4382d70bce085067722f8673a2d8d96b32bc
marceloserpa <jdev.marcelo@gmail.com>
2023-02-21 18:52:40 -0300

    spring security + Angular: init

---
+++ spring-security-angular/auth-token-based/src/main/java/com/marceloserpa/authtokenbased/AuthTokenBasedApplication.java
@@ -0,0 +1,13 @@
+package com.marceloserpa.authtokenbased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTokenBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthTokenBasedApplication.class, args);
+	}
+
+}
---
+++ spring-security-angular/auth-token-based/src/main/java/com/marceloserpa/authtokenbased/HelloRestController.java
@@ -0,0 +1,15 @@
+package com.marceloserpa.authtokenbased;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloRestController {
+
+    @GetMapping("/hello")
+    public String ok() {
+        return "ooi";
+    }
+
+}
