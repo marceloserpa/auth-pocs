b023a08efe5e20db5a9f493c9ffc90de795b539f
marceloserpa <jdev.marcelo@gmail.com>
2023-02-24 23:22:26 -0300

    implement http interceptor

--- spring-security-angular/front-app/src/app/app.module.ts
+++ spring-security-angular/front-app/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { LoginComponent } from './login/login.component';
+import { SecurityInterceptorService } from './security-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,10 @@ import { LoginComponent } from './login/login.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: SecurityInterceptorService, multi: true },
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- spring-security-angular/front-app/src/app/books-api.service.ts
+++ spring-security-angular/front-app/src/app/books-api.service.ts
@@ -12,9 +12,8 @@ export class BooksApiService {
   constructor(private httpClient:HttpClient) { }
 
   getBooks(): Observable<Book[]>{
-    const headers = { 'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYXJjZWxvIiwiZXhwIjoxNjc3Mjk5Nzg2LCJpYXQiOjE2NzcyODE3ODZ9.GCZjHg_YYFQQu5N6PilDbbt_CclMOkkadLbwsg70HIN9fnXgGZqVTG8mWp4jMeMzGysaLDY3sF_wTnHS850hnA', 'My-Custom-Header': 'foobar' }
     return this.httpClient.get<Book[]>(
-      "http://localhost:9000/api/books", {headers});
+      "http://localhost:9000/api/books");
   }
 
 }
